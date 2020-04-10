675c00ef5722b83facfcbfc7cc7d72c4bef598d7
Tung Do <tdo@haverford.edu>
2020-04-10 12:20:41 -0400

    second commit

--- flaskblog.py
+++ flaskblog.py
@@ -34,13 +34,13 @@ class Post(db.Model):
 
 posts = [
     {
-        'author': 'Corey Schafer',
+        'author': 'Tu',
         'title': 'Blog Post 1',
         'content': 'First post content',
         'date_posted': 'April 20, 2018'
     },
     {
-        'author': 'Jane Doe',
+        'author': 'Kim',
         'title': 'Blog Post 2',
         'content': 'Second post content',
         'date_posted': 'April 21, 2018'
